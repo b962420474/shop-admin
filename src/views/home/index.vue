af75d65f4080815aab95c3395284a567d75ffa15
b962420474 <1511033637@qq.com>
2021-09-21 16:06:34 +0800

    chore: 配置路径别名

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,7 +2,7 @@
   <div>首页</div>
 </template>
 <script lang="ts" setup>
-import { useStore } from '../../store'
+import { useStore } from '@/store'
 const store = useStore()
 console.log(store.state.count)
 </script>
